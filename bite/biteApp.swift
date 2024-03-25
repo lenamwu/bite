5b58566ae54131af6cfddf37f90354de7f77479d
lenawu1 <lena.m.wu@gmail.com>
2024-03-24 22:42:03 -0700

    Initial Commit

---
+++ bite/biteApp.swift
@@ -0,0 +1,17 @@
+//
+//  biteApp.swift
+//  bite
+//
+//  Created by Lena Wu on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct biteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
